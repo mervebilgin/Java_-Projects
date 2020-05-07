d13395bf6debed3e452f4da40d6ffbdb9cc03ef5
bilginmrv <47225405+mervebilgin@users.noreply.github.com>
2020-05-07 12:11:33 +0300

    Add files via upload

---
+++ Motor.java
@@ -0,0 +1,13 @@
+package ailearabasi;
+
+public class Motor {
+    
+    private static final int motor_gucu = 3600;
+    public void calis(){
+        System.out.println("Motor çalışıyor...");
+    }
+    public void dur(){
+        System.out.println("Motor durdu...");
+    }
+    
+}
