e26d7e086795f643019a614e3f546e8b8fcde036
bilginmrv <47225405+mervebilgin@users.noreply.github.com>
2020-06-29 17:24:42 +0300

    Update Test.java

--- Komposizyonİlişkisi/Test.java
+++ Komposizyonİlişkisi/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args){
         
         Resolution resolution = new Resolution(1920,1080); 
+        //Resolution objesi oluşturduk ^_^ ...
         Monitor monitor = new Monitor("VS197DE", "ASUS", "18.5", resolution);
         
         Kasa kasa = new Kasa("Shadow Blade", "Shadow", "Temperli Cam");
@@ -17,8 +18,6 @@ public class Test {
         pc.getAnakart().isletim_sistemi_yükle("Ubuntu 16.04"); 
         
         monitor.bilgileriGoster();
-        
-        
     }
     
 }
